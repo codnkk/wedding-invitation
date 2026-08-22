ccc40b296174f80fb006feae0ab3acb2d7d3b950
codnkk <codnkk@gmail.com>
2026-08-22 20:43:33 +0900

    Update index.tsx

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -52,7 +52,7 @@ export const Location = () => {
             - 급행(빨강): 8601, 2500, 8600
             <br />
             - 공항(검정): 6015, 6021, 6702
-            <span style='color-black'>공항버스</span> 6015, 6021, 6702
+            <span style="color : black">공항버스</span> 6015, 6021, 6702
           </div>
         </div>
 
