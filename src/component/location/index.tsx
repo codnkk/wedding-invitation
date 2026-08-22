f1149c913376c6729f6b3bef60f4a27a0aef2300
codnkk <codnkk@gmail.com>
2026-08-22 21:38:15 +0900

    update location

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -33,25 +33,25 @@ export const Location = () => {
           <div className="heading">대중교통</div>
           <div />
           <div className="content">
-            * 지하철 이용시
+            * 지하철
             <br />
-            5, 6호선 <b>공덕역 8번 출구</b> (도보 2분)
+            &nbsp; 5, 6호선 <b>공덕역 8번 출구</b> (도보 2분)
             <br />
-            5호선 <b>마포역 3번 출구</b> (도보 5분)
+            &nbsp; 5호선 <b>마포역 3번 출구</b> (도보 5분)
             <br />
-            경의선, 공항철도 <b>공덕역 9번 출구</b> (도보 2분)
+            &nbsp; 경의선, 공항철도 <b>공덕역 9번 출구</b> (도보 2분)
           </div>
           <div />
           <div className="content">
-            * 버스 이용 시
+            * 버스
             <br />
-            - 간선(파랑): 160, 260, 463, 600
+            &nbsp; <span className="content-blue">간선버스</span> 160, 260, 463, 600
             <br />
-            - 지선(초록): 7013A, 7013B, 7016, 7611, 7613
+            &nbsp; <span className="content-green">지선버스</span> 7013A, 7013B, 7016, 7611, 7613
             <br />
-            - 급행(빨강): 8601, 2500, 8600
+            &nbsp; <span className="content-red">급행버스</span> 8601, 2500, 8600
             <br />
-            - 공항(검정): 6015, 6021, 6702
+            &nbsp; <span className="content-black">공항버스</span> 6015, 6021, 6702
           </div>
         </div>
 
@@ -65,16 +65,17 @@ export const Location = () => {
           <div className="content">
             네이버 지도, 카카오 네비, 티맵 등 이용
             <br />
-            <b>서울특별시 마포구 마포대로 78 경찰공제회 자람빌딩 이룸웨딩컨벤션</b> 검색
+            <b>서울시 마포구 마포대로 78 경찰공제회 자람빌딩
+            <br /> 이룸웨딩컨벤션</b> 검색
             <br />
             - 주차 요금은 2시간 무료입니다.
             <br />
-            (안내데스크에서 주차 등록)
+            &nbsp; (안내데스크에서 주차 등록)
           </div>
           <div />
           <div className="content">
-            <b>
-              ※ 주차장 층간 진입로가 협소하오니, 운전에 유의바랍니다.
+            <b> ※ 주차장 층간 진입로가 협소하오니, 
+            <br /> &nbsp; &nbsp;운전에 유의바랍니다.
             </b>
           </div>
         </div>
